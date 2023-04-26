453fef3c65dddc0a68fc24963178c26d4c610b49
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-04-26 11:21:00 -0300

    Styes: theme added

--- itoddy-gym/App.tsx
+++ itoddy-gym/App.tsx
@@ -1,18 +1,30 @@
 import { StatusBar, Text, View } from "react-native";
 
+import { NativeBaseProvider } from "native-base";
+
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import React from "react";
+import { Loading } from "./src/Components/Loading";
+
+import { THEME } from "./src/theme";
 
 export default function App() {
   const fontsLoaded = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: 'center', backgroundColor: '#202024'}}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
-    </View>
+    <NativeBaseProvider theme={THEME}>
+
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        {!fontsLoaded ? <Text>Hello World!</Text> : <Loading/>}
+    </NativeBaseProvider>
   );
 }

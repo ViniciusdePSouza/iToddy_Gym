6ad886d2d5f8f7b303728c082e6d07c592dafb47
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-05-03 11:06:42 -0300

    Styles: bottom tab in progress. Navigation: navigator by bottom tab works

---
+++ itoddy-gym/src/routes/app.routes.tsx
@@ -0,0 +1,76 @@
+import {
+  createMaterialBottomTabNavigator,
+  MaterialBottomTabNavigationProp,
+} from "@react-navigation/material-bottom-tabs";
+
+import { Home } from "@screens/Home";
+import { History } from "@screens/History";
+import { Profile } from "@screens/Profile";
+import { Exercise } from "@screens/Exercise";
+
+import HomeSvg from "@assets/home.svg";
+import HistorySvg from "@assets/history.svg";
+import ProfileSvg from "@assets/profile.svg";
+import { useTheme } from "native-base";
+
+type AppRoutes = {
+  home: undefined;
+  history: undefined;
+  profile: undefined;
+  exercise: undefined;
+};
+
+export type AppNavigatorRoutesProps =
+  MaterialBottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createMaterialBottomTabNavigator<AppRoutes>();
+
+export function AppRoutes() {
+  const { sizes, colors } = useTheme();
+
+  const IconSize = sizes[6];
+
+  return (
+    <Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: { backgroundColor: colors.gray },
+        tabBarActiveTintColor: colors.yellow[500],
+        tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: { backgroundColor: colors.gray[600] },
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={IconSize} height={IconSize} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={IconSize} height={IconSize} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={IconSize} height={IconSize} />
+          ),
+        }}
+      />
+
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  );
+}
---
+++ itoddy-gym/src/screens/Exercise.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export function Exercise() {
+    return (
+        <Center flex={1}>
+            <Text color='white'>Exercise works!</Text>
+        </Center>
+    )
+}

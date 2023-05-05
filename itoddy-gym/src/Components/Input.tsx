f2b8456ff6849798c87b161e7471fcc9daa406c1
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-05-05 11:35:19 -0300

    Feature: signUp Form done

--- itoddy-gym/src/Components/Input.tsx
+++ itoddy-gym/src/Components/Input.tsx
@@ -1,23 +1,43 @@
-import { Input as NativeBaseInput, IInputProps } from "native-base";
+import {
+  Input as NativeBaseInput,
+  IInputProps,
+  FormControl,
+} from "native-base";
 
-export function Input({ ...rest }: IInputProps) {
+type InputProps = IInputProps & {
+  errorMessage?: string | null;
+};
+
+export function Input({ errorMessage = null, isInvalid, ...rest }: InputProps) {
+  const invalidInput = !!errorMessage || isInvalid;
+
   return (
-    <NativeBaseInput
-      bg="gray.700"
-      h={14}
-      px={14}
-      borderWidth={0}
-      fontSize="md"
-      color="white"
-      fontFamily="body"
-      mb={4}
-      placeholderTextColor="gray.300"
-      _focus={{
-        bg: 'gray.700',
-        borderWidth: 1,
-        borderColor: "yellow.500"
-      }}
-      {...rest}
-    />
+    <FormControl isInvalid={invalidInput} mb={4}>
+      <NativeBaseInput
+        bg="gray.700"
+        h={14}
+        px={14}
+        borderWidth={0}
+        fontSize="md"
+        color="white"
+        fontFamily="body"
+        placeholderTextColor="gray.300"
+        _focus={{
+          bg: "gray.700",
+          borderWidth: 1,
+          borderColor: "yellow.500",
+        }}
+        isInvalid={invalidInput}
+        _invalid={{
+          borderWidth: 1,
+          borderColor: "red.500",
+        }}
+        {...rest}
+      />
+
+      <FormControl.ErrorMessage>
+        {errorMessage}
+      </FormControl.ErrorMessage>
+    </FormControl>
   );
 }
--- itoddy-gym/src/screens/SignUp.tsx
+++ itoddy-gym/src/screens/SignUp.tsx
@@ -5,13 +5,55 @@ import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 import { Input } from "../Components/Input";
 import { Button } from "../Components/Button";
+
 import { useNavigation } from "@react-navigation/native";
 
+import { Controller, useForm } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import * as yup from "yup";
+
+type FormDataSchema = {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirmed: string;
+};
+
+const signUpSchema = yup.object({
+  name: yup.string().required("Informe o nome"),
+  email: yup.string().required("Informe o email").email("Email inválido"),
+  password: yup
+    .string()
+    .required("Informe a senha")
+    .min(6, "A senha deve ter no mínimo 6 caracteres"),
+  passwordConfirmed: yup
+    .string()
+    .required("Confirme a senha")
+    .oneOf(
+      [yup.ref('password')],
+      "A senha de confirmação não bate com a senha informada"
+    ),
+});
+
 export function SignUp() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataSchema>({
+    resolver: yupResolver(signUpSchema),
+  });
+
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp(data: FormDataSchema) {
+    console.log(data);
   }
 
   return (
@@ -41,17 +83,69 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input placeholder="Nome" />
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
 
-          <Input placeholder="Senha" secureTextEntry />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="passwordConfirmed"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                placeholder="Confirme a Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+                errorMessage={errors.passwordConfirmed?.message}
+              />
+            )}
+          />
         </Center>
-        <Button title="Criar e acessar" />
+        <Button title="Criar e acessar" onPress={handleSubmit(handleSignUp)} />
 
         <Button
           title="Voltar para o login"
